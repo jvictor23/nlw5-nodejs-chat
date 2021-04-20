a8c350d0a67ba410273a2ca17e1309d7ea3a29a3
João Victor da Silva <joaovictor00788123@gmail.com>
2021-04-20 18:32:01 -0300

    Dia 2

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 
+import "./database";
+import {routes} from './routes'
+
 const app = express();
 
-app.use('/',(req,res)=>{
-    res.send("Olá Mundo")
-})
+app.use(express.json());
+
+app.use(routes);
 
 app.listen(3000, ()=> console.log("Server is running on port 3000"));
 
