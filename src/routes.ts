008b6919f7390ba7b5508c61456170325abdeece
João Victor da Silva <joaovictor00788123@gmail.com>
2021-04-22 13:39:24 -0300

    Dia 3

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,22 @@
 import {Router} from 'express';
 import { getCustomRepository } from 'typeorm';
+import { MessageController } from './controllers/MessageController';
 import { SettingsController } from './controllers/SettingsController';
+import { UserController } from './controllers/UserController';
 import { SettingsRepository } from './repositories/SettingsRepository';
 
 const routes = Router();
 
 const settingsController = new SettingsController();
+const usersController =  new UserController();
+const messageController =  new MessageController();
 
-routes.post("/settings", settingsController.create)
+routes.post("/settings", settingsController.create);
+
+routes.post("/users", usersController.create);
+
+routes.post("/messages", messageController.create);
+routes.get("/messages/:id", messageController.showByUser);
+
 
 export {routes}
